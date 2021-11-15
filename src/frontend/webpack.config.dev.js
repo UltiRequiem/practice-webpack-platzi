2231e23bb9252cd05811d4baa329dea5c660a400
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-15 13:40:10 -0500

    :recycle: Update

--- src/frontend/webpack.config.dev.js
+++ src/frontend/webpack.config.dev.js
@@ -2,17 +2,14 @@ const path = require('node:path');
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerseExtractPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -46,6 +43,10 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: './public/index.html',
@@ -54,24 +55,5 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
-    new CleanWebpackPlugin(),
   ],
-
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerseExtractPlugin({
-        test: /\.js(\?.*)?$/i,
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-      }),
-      new CSSMinimizerPlugin({
-        test: /\.css$/i,
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-      }),
-    ],
-  },
 };
