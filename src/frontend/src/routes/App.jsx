43de2d1dfdebf6df3be8e2dcb88d8048e4e68f79
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-15 13:26:24 -0500

    :alien: Update Webpack and React

--- src/frontend/src/routes/App.jsx
+++ src/frontend/src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
@@ -8,11 +8,11 @@ import NotFound from '../containers/NotFound';
 const App = () => (
   <BrowserRouter>
     <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/checkout" element={Checkout} />
+        <Route element={NotFound} />
+      </Routes>
     </Layout>
   </BrowserRouter>
 );
--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('node:path');
+
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
